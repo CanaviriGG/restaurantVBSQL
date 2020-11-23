4e58185c0175be27477b1907b946ad6ed433fbd1
CanaviriGG <73559840+CanaviriGG@users.noreply.github.com>
2020-11-23 17:51:19 -0400

    Agregar archivos de proyecto.

---
+++ Panelde_Admi_software/Conexion_Manual.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Conexion_Manual
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCnString = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(92, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(302, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingresar la cadena de conexion local"
+        Me.Label1.UseWaitCursor = True
+        '
+        'Label2
+        '
+        Me.Label2.Location = New System.Drawing.Point(93, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(441, 50)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Una vez que este listo dale a ""generar cadena de conexion"" se creara un archivo q" &
+    "ue contendra tu conexion Encryptada. Ahora tu conexion es mas segura ante posibl" &
+    "es hacks"
+        '
+        'txtCnString
+        '
+        Me.txtCnString.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCnString.Location = New System.Drawing.Point(96, 126)
+        Me.txtCnString.Name = "txtCnString"
+        Me.txtCnString.Size = New System.Drawing.Size(364, 26)
+        Me.txtCnString.TabIndex = 2
+        Me.txtCnString.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.RoyalBlue
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(96, 184)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(308, 44)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Generar cadena de conexion"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Conexion_Manual
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 259)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtCnString)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Conexion_Manual"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Una vez que este listo dale a ""generar cadena de conexion"" se creara un archivo q" &
+    "ue contendra tu conexion Encryptada. "
+        Me.UseWaitCursor = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCnString As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Panelde_Admi_software/Conexion_Manual.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.Xml
+Public Class Conexion_Manual
+    Private aes As New AES()
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtCnString.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SavetoXML(aes.Encrypt(txtCnString.Text, appPwdUnique, Integer.Parse("256")))
+
+    End Sub
+    Public Sub SavetoXML(ByVal dbcnString)
+        Dim doc As XmlDocument = New XmlDocument()
+        doc.Load("ConnectionString.xml")
+        Dim root As XmlElement = doc.DocumentElement
+        root.Attributes.Item(0).Value = dbcnString
+        Dim writer As XmlTextWriter = New XmlTextWriter("ConnectionString.xml", Nothing)
+        writer.Formatting = Formatting.Indented
+        doc.Save(writer)
+        writer.Close()
+    End Sub
+    Dim dbcnString As String
+    Public Sub ReadfromXML()
+        Try
+            Dim doc As XmlDocument = New XmlDocument()
+            doc.Load("ConnectionString.xml")
+            Dim root As XmlElement = doc.DocumentElement
+            dbcnString = root.Attributes.Item(0).Value
+            txtCnString.Text = (aes.Decrypt(dbcnString, appPwdUnique, Integer.Parse("256")))
+        Catch ex As System.Security.Cryptography.CryptographicException
+        End Try
+    End Sub
+
+    Private Sub Conexion_Manual_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReadfromXML()
+    End Sub
+End Class
